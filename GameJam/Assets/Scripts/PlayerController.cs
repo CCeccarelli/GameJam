9b9367ea5aa1e30dd4b6c479fa76e1a711e28551
Christian <christian.ceccarelli@ontariotechu.net>
2019-11-08 21:41:13 -0500

    Base scene with list of projectiles

--- GameJam/Assets/Scripts/PlayerController.cs
+++ GameJam/Assets/Scripts/PlayerController.cs
@@ -4,16 +4,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
+    public GameObject spawnLocation;
+    public GameObject gift;
+    GameObject temp;
+    public List<GameObject> people;
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < 5; i++)
+        {
+            people.Add(Instantiate(gift, spawnLocation.transform.position, Quaternion.identity));
+            
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonUp(0))
+        {
+            fire();
+            Debug.Log("test");
+        }
+    }
+
+    void fire()
+    {
+        //temp.transform.Translate(1, 0, 0);
+
     }
 }
