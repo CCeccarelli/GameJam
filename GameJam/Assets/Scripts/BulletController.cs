38477b4893a9b1b1657eac93d1bb55cd85ea0892
Christian <christian.ceccarelli@ontariotechu.net>
2019-11-09 13:31:19 -0500

    added object pooling for bullets

---
+++ GameJam/Assets/Scripts/BulletController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public BulletManager pc;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void moveBullet()
+    {
+        //rb.AddForce();
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == "Enemy")
+        {
+            Debug.Log("hit");
+        }
+    }
+}
---
+++ GameJam/Assets/Scripts/BulletManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletManager : MonoBehaviour
+{
+    public GameObject gift;
+    GameObject temp;
+    public List<GameObject> people;
+    public int sizeList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i <= sizeList; i++)
+        {
+            people.Add(Instantiate(gift, new Vector3(0,0,0), Quaternion.identity));
+            people[i].GetComponent<BulletController>().pc = this;
+            people[i].SetActive(false);
+            people[i].transform.name = "Bullet_" + i;
+        }
+        sizeList = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public GameObject getBullet()
+    {
+       if(sizeList > people.Count - 2)
+        {
+            sizeList = 0;
+        }
+        else
+        {
+            sizeList++;
+        }
+
+        return people[sizeList];
+            
+        
+    }
+
+    public void resetBullet(GameObject b)
+    {
+        b.SetActive(false);
+        b.transform.position = new Vector3(0, 0, 0);
+    }
+}
